19c78daddfe24dbdf8f045907488d9a54257d593
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-01-15 19:57:36 +0100

    Add publishing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `java-library`
+    id("net.kyori.indra")
+    id("net.kyori.indra.publishing")
 }
 
 repositories {
@@ -24,4 +26,52 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+indra {
+    github("SkinsRestorer", "axiom") {
+        ci(true)
+    }
+
+    gpl3OnlyLicense()
+    publishReleasesTo("codemc-releases", "https://repo.codemc.org/repository/maven-releases/")
+    publishSnapshotsTo("codemc-snapshots", "https://repo.codemc.org/repository/maven-snapshots/")
+
+    configurePublications {
+        pom {
+            name.set("axiom")
+            url.set("https://skinsrestorer.net/")
+            organization {
+                name.set("SkinsRestorer")
+                url.set("https://skinsrestorer.net")
+            }
+            developers {
+                developer {
+                    id.set("xknat")
+                    timezone.set("Europe/Amsterdam")
+                }
+                developer {
+                    id.set("AlexProgrammerDE")
+                    timezone.set("Europe/Berlin")
+                    url.set("https://pistonmaster.net")
+                }
+            }
+        }
+
+        versionMapping {
+            usage(Usage.JAVA_API) { fromResolutionOf(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME) }
+            usage(Usage.JAVA_RUNTIME) { fromResolutionResult() }
+        }
+    }
+}
+
+val repoName = if (version.toString().endsWith("SNAPSHOT")) "maven-snapshots" else "maven-releases"
+publishing {
+    repositories {
+        maven("https://repo.codemc.org/repository/${repoName}/") {
+            credentials.username = System.getenv("CODEMC_USERNAME")
+            credentials.password = System.getenv("CODEMC_PASSWORD")
+            name = "codemc"
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+        id("net.kyori.indra") version "2.0.6"
+        id("net.kyori.indra.git") version "2.0.6"
+        id("net.kyori.indra.publishing") version "2.0.6"
+    }
+}
+
 rootProject.name = "axiom"
 include("lib")
