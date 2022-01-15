1ba4652926232e505b9cf26c053a19097f9c9297
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-01-15 20:26:34 +0100

    Add group id

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,6 +9,9 @@ repositories {
     mavenCentral()
 }
 
+group = "net.skinsrestorer"
+version = "1.0.0"
+
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
 
